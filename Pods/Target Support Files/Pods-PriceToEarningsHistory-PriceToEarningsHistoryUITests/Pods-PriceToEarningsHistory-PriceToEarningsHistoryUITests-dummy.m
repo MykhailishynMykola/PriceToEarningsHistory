a2d0a3adf0067cbd78f5162241acc3ad76e2e785
Mykhailishyn Mykola <nikolas90-90@ukr.net>
2023-08-08 15:45:28 +0100

    Pod install

---
+++ Pods/Target Support Files/Pods-PriceToEarningsHistory-PriceToEarningsHistoryUITests/Pods-PriceToEarningsHistory-PriceToEarningsHistoryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PriceToEarningsHistory_PriceToEarningsHistoryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PriceToEarningsHistory_PriceToEarningsHistoryUITests
+@end
---
+++ Pods/Target Support Files/Pods-PriceToEarningsHistory/Pods-PriceToEarningsHistory-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PriceToEarningsHistory : NSObject
+@end
+@implementation PodsDummy_Pods_PriceToEarningsHistory
+@end
