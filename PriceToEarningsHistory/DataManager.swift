3d313ba4fd5687dd1b0154c0d813ed2ef3a2070e
Mykhailishyn Mykola <nikolas90-90@ukr.net>
2023-08-10 10:01:28 +0100

    Whitelist handling

--- PriceToEarningsHistory/DataManager.swift
+++ PriceToEarningsHistory/DataManager.swift
@@ -17,6 +17,15 @@ final class DataManager {
     private var symbolsCache: [CompanySymbol] = []
     private var basicFinancialsCache: [String: BasicFinancials] = [:]
     
+    private var whitelist: [String] {
+        guard let path = Bundle.main.path(forResource: "whitelist", ofType: "json"),
+              let jsonData = try? String(contentsOfFile: path).data(using: .utf8) else {
+            return []
+        }
+        let result = (try? JSONDecoder().decode([String].self, from: jsonData)) ?? []
+        return result
+    }
+    
     private init() {}
     
     func getSymbols(completion: @escaping ([CompanySymbol]) -> ())  {
@@ -37,27 +46,40 @@ final class DataManager {
             completion(basicFinancialsCache[symbol]!)
             return
         }
-        let loadedBasicFinancials = loadBasicFinancialsFromFile(for: symbol)
-        guard loadedBasicFinancials == nil else {
-            completion(loadedBasicFinancials!)
+        let loaded = loadBasicFinancialsFromFile(for: symbol)
+        guard loaded == nil else {
+            completion(loaded!)
             return
         }
         updateBasicFinancials(for: symbol, completion: completion)
+    }
+    
+    func getCachedFundamentals(completion: @escaping ([String: BasicFinancials]) -> ()) {
+        guard basicFinancialsCache.isEmpty else {
+            completion(basicFinancialsCache)
+            return
+        }
+        let loaded = loadBasicFinancialsFromFile()
+        completion(loaded)
+        return
     }
 }
 
 private extension DataManager {
     func updateSymbols(completion: @escaping ([CompanySymbol]) -> ()) {
         let jsonEncoder = JSONEncoder()
+        let whitelist = self.whitelist
         
         FinnhubClient.symbols(exchange: .unitedStates) { result in
             switch result {
             case let .success(symbols):
-                for symbol in symbols {
+                let filteredSymbols = symbols.filter { whitelist.contains($0.symbol) }
+                for symbol in filteredSymbols {
+                    guard whitelist.contains(symbol.symbol) else { continue }
                     let jsonData = try! jsonEncoder.encode(symbol)
                     let jsonString = String(data: jsonData, encoding: .utf8)!
                     
-                    let pathWithFilename = Self.documentDirectory.appendingPathComponent("\(symbol.displaySymbol)_smbl.json")
+                    let pathWithFilename = Self.documentDirectory.appendingPathComponent("\(symbol.symbol)_smbl.json")
                     do {
                         try jsonString.write(to: pathWithFilename,
                                              atomically: true,
@@ -66,8 +88,8 @@ private extension DataManager {
                         print(error)
                     }
                 }
-                self.symbolsCache = symbols
-                completion(symbols)
+                self.symbolsCache = filteredSymbols
+                completion(filteredSymbols)
             case .failure(.invalidData):
                 print("Invalid data")
             case let .failure(.networkFailure(error)):
@@ -143,5 +165,27 @@ private extension DataManager {
             print(error)
         }
         return nil
+    }
+    
+    func loadBasicFinancialsFromFile() -> [String: BasicFinancials] {
+        do {
+            var result: [String: BasicFinancials] = [:]
+            let fileNames = try FileManager.default.contentsOfDirectory(atPath: Self.documentDirectory.path)
+            for fileName in fileNames {
+                guard fileName.contains("_bf.json") else {
+                    continue
+                }
+                let url = Self.documentDirectory.appendingPathComponent(fileName)
+                if let jsonData = try String(contentsOfFile: url.path).data(using: .utf8) {
+                    let bf = try JSONDecoder().decode(BasicFinancials.self, from: jsonData)
+                    let symbol = String(fileName.dropLast("_bf.json".count))
+                    result[symbol] = bf
+                }
+            }
+            return result
+        } catch {
+            print(error)
+        }
+        return [:]
     }
 }
--- PriceToEarningsHistory/SymbolsViewController.swift
+++ PriceToEarningsHistory/SymbolsViewController.swift
@@ -26,36 +26,65 @@ final class SymbolsViewController: UIViewController {
     }
     fileprivate var symbolsInProgress: [String] = []
     private var timer: Timer?
+    private var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction private func loadButtonPressed(_ sender: Any) {
+        view.isUserInteractionEnabled = false
         DataManager.shared.getSymbols { [weak self] symbols in
-            guard let self = self else { return }
-            self.symbols = symbols.sorted(by: { $0.displaySymbol < $1.displaySymbol })
-            
-            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.timerFired), userInfo: nil, repeats: true)
-            self.timer?.fire()
+            self?.symbols = symbols.sorted(by: { $0.symbol < $1.symbol })
+            DispatchQueue.main.async { [weak self] in
+                self?.view.isUserInteractionEnabled = true
+            }
         }
     }
     
-    private var counter = 0
-    @objc private func timerFired() {
+    @IBAction private func loadNumbersButtonPressed(_ sender: Any) {
+        view.isUserInteractionEnabled = false
+        DataManager.shared.getCachedFundamentals { [weak self] basicFinancials in
+            self?.basicFinancials = basicFinancials
+            DispatchQueue.main.async { [weak self] in
+                self?.view.isUserInteractionEnabled = true
+            }
+            self?.startTimer()
+        }
+    }
+}
+
+private extension SymbolsViewController {
+    // MARK: - Timer
+    
+    func startTimer() {
+        let timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.timerFired), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer, forMode: .common)
+        timer.fire()
+        self.timer = timer
+    }
+    
+    @objc func timerFired() {
         guard symbols.indices.contains(counter) else {
+            symbolsInProgress = []
             timer?.invalidate()
             return
         }
-        var nextSymbol = symbols[counter].displaySymbol
+        var nextSymbol = symbols[counter].symbol
         while basicFinancials.keys.contains(nextSymbol) || symbolsInProgress.contains(nextSymbol) {
             counter += 1
             
             guard symbols.indices.contains(counter) else {
+                symbolsInProgress = []
                 timer?.invalidate()
                 return
             }
-            nextSymbol = symbols[counter].displaySymbol
+            nextSymbol = symbols[counter].symbol
         }
         symbolsInProgress.append(nextSymbol)
+        print("BF loading: \(counter+1). \(nextSymbol)")
         DataManager.shared.getFundamentals(symbol: nextSymbol) { [weak self] basicFinancials in
             self?.basicFinancials[nextSymbol] = basicFinancials
         }
@@ -72,8 +101,23 @@ extension SymbolsViewController: UITableViewDataSource {
             return UITableViewCell()
         }
         let symbol = symbols[indexPath.item]
-        let peAnnual = basicFinancials[symbol.displaySymbol]?.metric.peAnnual ?? 0
-        cell.title = "\(indexPath.item+1). \(symbol.displaySymbol) - \(peAnnual)"
+        let peAnnual = basicFinancials[symbol.symbol]?.metric.peAnnual ?? 0
+        
+        var peHistory: Float = 0
+        if let historyNotes = basicFinancials[symbol.symbol]?.series.annual.pe {
+            var sum: Float = 0
+            for note in historyNotes {
+                sum += note.value
+            }
+            if historyNotes.count != 0 {
+                peHistory = sum / Float(historyNotes.count)
+            }
+        }
+        if peHistory != 0 {
+            cell.title = "\(indexPath.item+1). \(symbol.symbol) - p/e: \(peAnnual) h_p/e: \(peHistory)"
+        } else {
+            cell.title = "\(indexPath.item+1). \(symbol.symbol) - p/e: \(peAnnual)"
+        }
         return cell
     }
 }
